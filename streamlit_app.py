b749b7806fd8c9bd21ec91ef2983adf868f539da
elihrda <277870@vutbr.cz>
2025-10-01 11:15:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib.pagesizes import A4
 import io
@@ -31,7 +31,6 @@ ax.set_xlabel("x [m]")
 ax.set_ylabel("y [m]")
 ax.grid(True)
 ax.set_title(f"Kružnice se {n} body")
-
 st.pyplot(fig)
 
 df = pd.DataFrame({"x [m]": x, "y [m]": y})
@@ -39,15 +38,15 @@ st.dataframe(df)
 
 with st.expander("Informace o autorovi a použitých technologiích"):
     st.write("""
-    **Autor:** Eliška Hrdá
-    **Kontakt:** 277870@vutbr.cz
+    **Autor:** Eliška Hrdá  
+    **Kontakt:** 277870@vutbr.cz  
 
     **Použité technologie:**  
-    - [Streamlit](https://streamlit.io/) pro vývoj webové aplikace  
-    - [NumPy](https://numpy.org/) pro výpočty  
-    - [Matplotlib](https://matplotlib.org/) pro graf  
-    - [Pandas](https://pandas.pydata.org/) pro tabulku dat  
-    - [ReportLab](https://www.reportlab.com/) pro export do PDF  
+    - [Streamlit](https://streamlit.io/)  
+    - [NumPy](https://numpy.org/)  
+    - [Matplotlib](https://matplotlib.org/)  
+    - [Pandas](https://pandas.pydata.org/)  
+    - [ReportLab](https://www.reportlab.com/)  
     """)
 
 def create_pdf():
@@ -55,20 +54,28 @@ def create_pdf():
     doc = SimpleDocTemplate(buffer, pagesize=A4)
     styles = getSampleStyleSheet()
     elements = []
-
     elements.append(Paragraph("Generátor bodů na kružnici", styles["Title"]))
     elements.append(Spacer(1, 12))
-
-    elements.append(Paragraph(f"Parametry:", styles["Heading2"]))
+    elements.append(Paragraph("Parametry:", styles["Heading2"]))
     elements.append(Paragraph(f"Střed: ({x0}, {y0}) m", styles["Normal"]))
     elements.append(Paragraph(f"Poloměr: {r} m", styles["Normal"]))
     elements.append(Paragraph(f"Počet bodů: {n}", styles["Normal"]))
     elements.append(Paragraph(f"Barva bodů: {barva}", styles["Normal"]))
     elements.append(Spacer(1, 12))
-
-    elements.append(Paragraph("Autor: Vaše jméno", styles["Normal"]))
-    elements.append(Paragraph("Kontakt: váš@email.cz", styles["Normal"]))
-
+    img_buffer = io.BytesIO()
+    fig, ax = plt.subplots(figsize=(4,4))
+    ax.plot(x_closed, y_closed, marker="o", color=barva)
+    ax.set_aspect("equal", adjustable="box")
+    ax.set_xlabel("x [m]")
+    ax.set_ylabel("y [m]")
+    ax.grid(True)
+    fig.savefig(img_buffer, format="png")
+    plt.close(fig)
+    img_buffer.seek(0)
+    elements.append(Image(img_buffer))
+    elements.append(Spacer(1, 12))
+    elements.append(Paragraph("Autor: Eliška Hrdá", styles["Normal"]))
+    elements.append(Paragraph("Kontakt: 277870@vutbr.cz", styles["Normal"]))
     doc.build(elements)
     buffer.seek(0)
     return buffer
